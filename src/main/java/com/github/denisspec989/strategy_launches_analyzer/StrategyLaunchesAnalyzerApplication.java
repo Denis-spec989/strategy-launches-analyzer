a252616d13de58cfa312545b2391bd89d9c01b59
IonkinaM <marinkay666@gmail.com>
2026-06-01 15:59:13 +0300

    first commit

---
+++ src/main/java/com/github/denisspec989/strategy_launches_analyzer/StrategyLaunchesAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.github.denisspec989.strategy_launches_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyLaunchesAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategyLaunchesAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/denisspec989/strategy_launches_analyzer/StrategyLaunchesAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.denisspec989.strategy_launches_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyLaunchesAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
